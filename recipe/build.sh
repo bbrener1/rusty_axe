d1e7ae24956a7e257a5c1cf78bdc02c17aeb3f47
bbrener1 <bbrener1@jhu.edu>
2021-10-25 19:48:57 -0400

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,15 @@
 # used by the setuptools install process and alerts it to the location
 # of the library.
 
+echo $(ls)
+echo $(ls ./*)
+echo $(pwd)
 python -m build
 
 echo $(ls)
 echo $(ls ./*)
 
-pip install ./dist/rusty-axe-bbrener1-0.66.tar.gz
+python -m install ./dist/rusty-axe-bbrener1-0.66.tar.gz
 
 if [[ -f ./target/release/rf_5 ]];
 then
